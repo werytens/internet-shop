ce64b2be87ffe256716d9af11ea43063862702ff
Липатов Максим <103856799+aovent@users.noreply.github.com>
2023-06-08 22:23:05 +0300

    Create database.js

---
+++ backend/src/database.js
@@ -0,0 +1,13 @@
+import mysql from "mysql";
+
+export const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "internet_shop_lipatov",
+});
+
+connection.connect();
+connection.query("CREATE TABLE IF NOT EXISTS `clients` (id INT PRIMARY KEY AUTO_INCREMENT, FCS VARCHAR(100), createDate VARCHAR(200), updateDate VARCHAR(200), contacts VARCHAR(255));", (err) => {
+    if (err) throw err;
+});
