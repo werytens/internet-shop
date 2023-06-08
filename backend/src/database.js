9ea0b5a1007d519c9b767fd5039427862850f22e
aovent <maxim.lipatov06@gmail.com>
2023-06-08 22:27:50 +0300

    first commit

--- backend/src/database.js
+++ backend/src/database.js
@@ -11,3 +11,6 @@ connection.connect();
 connection.query("CREATE TABLE IF NOT EXISTS `clients` (id INT PRIMARY KEY AUTO_INCREMENT, FCS VARCHAR(100), createDate VARCHAR(200), updateDate VARCHAR(200), contacts VARCHAR(255));", (err) => {
     if (err) throw err;
 });
+
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'YourRootPassword';
