d7d375d7290c7a2f0eba8509975b4a5014877df0
Липатов Максим <103856799+aovent@users.noreply.github.com>
2023-06-08 22:19:37 +0300

    Create api.js

---
+++ frontend/assets/js/api.js
@@ -0,0 +1,26 @@
+const API_URL = "http://localhost:3000";
+
+export async function getItems() {
+    const resp = await fetch(API_URL);
+    return await resp.json();
+}
+
+export async function createItem(text) {
+    console.log(text);
+
+    const resp = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+    });
+
+    return await resp.json();
+}
+
+export async function deleteItem(id) {
+    const resp = await fetch(`${API_URL}/?id=${id}`, { method: "DELETE" });
+    return await resp.json();
+}
