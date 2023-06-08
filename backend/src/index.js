4932dd4c08b41decea58745fcbc603a7fbfa7454
Липатов Максим <103856799+aovent@users.noreply.github.com>
2023-06-08 22:23:18 +0300

    Create index.js

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import createRoute from "./routes/post.js";
+import getRoute from "./routes/get.js";
+import deleteRoute from "./routes/delete.js";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", getRoute);
+app.post("/", createRoute);
+app.delete("/", deleteRoute);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
