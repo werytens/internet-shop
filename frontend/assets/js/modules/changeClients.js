43277cc7d364fabafbf992b24edf3b8fe30375ec
aovent <maxim.lipatov06@gmail.com>
2023-06-16 22:50:20 +0300

    selectedContactsAdded

--- frontend/assets/js/modules/changeClients.js
+++ frontend/assets/js/modules/changeClients.js
@@ -6,6 +6,26 @@ export async function changeClients(data) {
 } 
 
 function setInfoForModal(info) {
+    let modalFields = document.querySelector(".modal_add_client").children; 
+    let contactsInModal = document.querySelector('.modal_add_client').querySelector(".contacts_in_modal");
+
+    info.FCS.split(" ").forEach((text, index) => {
+        modalFields[2].children[index].children[1].value = text;
+    })
+
+    let arrayForSelect = [];
+
+    let contacts = (info.contacts.slice(1)).slice(0, (info.contacts.length - 2)).split(",");
+    contacts.forEach(item => {
+        arrayForSelect.push(getIndexOfContact(item.split(":")[0].slice(1).slice(0, item.split(":")[0].slice(1).length - 1)) - 1)
+        
+        newContactAdd(contactsInModal);
+    })
+
+    setSelected(arrayForSelect);
+}
+
+function getIndexOfContact(contactName) {
     const contactsNamesDict = {
         "Телефон": "phoneNumber",
         "Email": "email",
@@ -14,16 +34,19 @@ function setInfoForModal(info) {
         "Другое": "other_contact"
     }
 
-    let modalFields = document.querySelector(".modal_add_client").children; 
-    let contactsInModal = document.querySelector('.modal_add_client').querySelector(".contacts_in_modal");
+    let indexOfItem = 1;
+    for (let index in contactsNamesDict) {
+        if (contactsNamesDict[index] == contactName) {
+            return indexOfItem;
+        }    
+        indexOfItem++
+    }
+}
 
-    info.FCS.split(" ").forEach((text, index) => {
-        modalFields[2].children[index].children[1].value = text;
-    })
+function setSelected(arrayForSelect) {
+    let modalContacts = document.querySelector(".modal_add_client").querySelectorAll("select");
 
-    let contacts = (info.contacts.slice(1)).slice(0, (info.contacts.length - 2)).split(",");
-    contacts.forEach(item => {
-        console.log(item)
-        newContactAdd(contactsInModal);
+    modalContacts.forEach((select, index) => {
+        console.log(select.selectedIndex = arrayForSelect[index])
     })
 }
